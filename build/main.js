19a5ff8296790ce33228328c94a5c715417d33d1
Sue Roh <sr3022@columbia.edu>
2017-03-28 00:04:48 -0400

    Yuh

---
+++ build/main.js
@@ -0,0 +1,6 @@
+document.getElementById("allison-obj-1").setAttribute("animation", "property: position; dir: alternate; dur: 1000; easing: easeInSine; loop: true; to: 0 2 0");
+document.getElementById("nicki-obj-1").setAttribute("animation", "property: position; dir: alternate; dur: 1000; easing: easeInSine; loop: true; to: -10 2 0");
+document.getElementById("jordan-obj-1").setAttribute("animation", "property: position; dir: alternate; dur: 1000; easing: easeInSine; loop: true; to: 10 2 0");
+document.getElementById("allison-obj-2").setAttribute("animation", "property: position; dir: alternate; dur: 1000; easing: easeInSine; loop: true; to: 10 2 40");
+document.getElementById("nicki-obj-2").setAttribute("animation", "property: position; dir: alternate; dur: 1000; easing: easeInSine; loop: true; to: 0 2 40");
+document.getElementById("jordan-obj-2").setAttribute("animation", "property: position; dir: alternate; dur: 1000; easing: easeInSine; loop: true; to: -10 2 40");
